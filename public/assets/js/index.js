49a73650766e74a9843c88928f07861cf3a4330d
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-02-03 01:35:47 +1100

    implemented server with get, post, delete function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -8,128 +8,160 @@ var $noteList = $(".list-container .list-group");
 var activeNote = {};
 
 // A function for getting all notes from the db
-var getNotes = function() {
-  return $.ajax({
-    url: "/api/notes",
-    method: "GET"
-  });
+var getNotes = function () {
+    var notesURL = '/api/notes';
+    return $.ajax({
+        url: notesURL,
+        method: "GET",
+        dataType: "json",
+        statusCode: {
+            404: function () {
+                alert("not found");
+                return;
+            }
+        }
+    });
+    // return $.ajax({
+    //     url: "/api/notes",
+    //     method: "GET"
+    // });
 };
 
 // A function for saving a note to the db
-var saveNote = function(note) {
-  return $.ajax({
-    url: "/api/notes",
-    data: note,
-    method: "POST"
-  });
+var saveNote = function (note) {
+    return $.post("/api/notes", note)
+        .then(function (data) {
+            console.log("notes.html", data);
+            alert("Adding note...");
+        });
+    // return $.ajax({
+    //     url: "/api/notes",
+    //     data: note,
+    //     method: "POST"
+    // });
 };
 
 // A function for deleting a note from the db
-var deleteNote = function(id) {
-  return $.ajax({
-    url: "api/notes/" + id,
-    method: "DELETE"
-  });
+var deleteNote = function (note) {
+    console.log(note);
+    return $.ajax({
+        url: "/api/notes/" + note,
+        method: "DELETE",   
+    })
+    .then(function (data) {
+        console.log("notes.html", data);
+        alert("Deleting note...");
+    });
 };
 
 // If there is an activeNote, display it, otherwise render empty inputs
-var renderActiveNote = function() {
-  $saveNoteBtn.hide();
+var renderActiveNote = function () {
+    $saveNoteBtn.hide();
 
-  if (activeNote.id) {
-    $noteTitle.attr("readonly", true);
-    $noteText.attr("readonly", true);
-    $noteTitle.val(activeNote.title);
-    $noteText.val(activeNote.text);
-  } else {
-    $noteTitle.attr("readonly", false);
-    $noteText.attr("readonly", false);
-    $noteTitle.val("");
-    $noteText.val("");
-  }
+    if (activeNote.routeName) {
+        $noteTitle.attr("readonly", true);
+        $noteText.attr("readonly", true);
+        $noteTitle.val(activeNote.title);
+        $noteText.val(activeNote.text);
+    } else {
+        $noteTitle.attr("readonly", false);
+        $noteText.attr("readonly", false);
+        $noteTitle.val("");
+        $noteText.val("");
+    }
 };
 
 // Get the note data from the inputs, save it to the db and update the view
-var handleNoteSave = function() {
-  var newNote = {
-    title: $noteTitle.val(),
-    text: $noteText.val()
-  };
+var handleNoteSave = function () {
+    var newNote = {
+        title: $noteTitle.val(),
+        text: $noteText.val()
+    };
 
-  saveNote(newNote).then(function(data) {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
+    saveNote(newNote).then(function (data) {
+        getAndRenderNotes();
+        renderActiveNote();
+    });
 };
 
 // Delete the clicked note
-var handleNoteDelete = function(event) {
-  // prevents the click listener for the list from being called when the button inside of it is clicked
-  event.stopPropagation();
+var handleNoteDelete = function (event) {
+    // prevents the click listener for the list from being called when the button inside of it is clicked
+    event.stopPropagation();
+    console.log('Delete button clicked');
+    var note = $(this)
+        .parent(".list-group-item")
+        .data();
+    var indexSelect = $(this).attr('dataIndex').trim()
+    console.log(note);
+    console.log(indexSelect);
+    console.log(note.routeName);
 
-  var note = $(this)
-    .parent(".list-group-item")
-    .data();
+    if (indexSelect === note.routeName) {
+        note = {};
+    }
 
-  if (activeNote.id === note.id) {
-    activeNote = {};
-  }
-
-  deleteNote(note.id).then(function() {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
+    deleteNote(indexSelect).then(function () {
+        getAndRenderNotes();
+        renderActiveNote();
+    });
 };
 
 // Sets the activeNote and displays it
-var handleNoteView = function() {
-  activeNote = $(this).data();
-  renderActiveNote();
+var handleNoteView = function () {
+    // console.log("active note clicked");
+    activeNote = $(this).data();
+    console.log(activeNote)
+    var activeNoteInput = $(this).attr('dataIndex').trim();
+    console.log(activeNoteInput);
+    renderActiveNote();
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
-var handleNewNoteView = function() {
-  activeNote = {};
-  renderActiveNote();
+var handleNewNoteView = function () {
+    activeNote = {};
+    renderActiveNote();
 };
 
 // If a note's title or text are empty, hide the save button
 // Or else show it
-var handleRenderSaveBtn = function() {
-  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
-    $saveNoteBtn.hide();
-  } else {
-    $saveNoteBtn.show();
-  }
+var handleRenderSaveBtn = function () {
+    if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+        $saveNoteBtn.hide();
+    } else {
+        $saveNoteBtn.show();
+    }
 };
 
 // Render's the list of note titles
-var renderNoteList = function(notes) {
-  $noteList.empty();
+var renderNoteList = function (notes) {
+    $noteList.empty();
 
-  var noteListItems = [];
+    var noteListItems = [];
 
-  for (var i = 0; i < notes.length; i++) {
-    var note = notes[i];
+    for (var i = 0; i < notes.length; i++) {
+        var note = notes[i];
 
-    var $li = $("<li class='list-group-item'>").data(note);
-    var $span = $("<span>").text(note.title);
-    var $delBtn = $(
-      "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
-    );
+        var $li = $("<li class='list-group-item'>").data(note);
+        $li.attr('dataIndex', note.routeName);
+        var $span = $("<span>").text(note.title);
+        var $delBtn = $(
+            "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+        );$delBtn.attr('dataIndex', note.routeName)
+        $
 
-    $li.append($span, $delBtn);
-    noteListItems.push($li);
-  }
+        $li.append($span, $delBtn);
+        noteListItems.push($li);
+    }
 
-  $noteList.append(noteListItems);
+    $noteList.append(noteListItems);
 };
 
 // Gets notes from the db and renders them to the sidebar
-var getAndRenderNotes = function() {
-  return getNotes().then(function(data) {
-    renderNoteList(data);
-  });
+var getAndRenderNotes = function () {
+    return getNotes().then(function (data) {
+        renderNoteList(data);
+    });
 };
 
 $saveNoteBtn.on("click", handleNoteSave);
--- server.js
+++ server.js
@@ -4,11 +4,20 @@ var express = require("express");
 var path = require("path");
 const fs = require("fs");
 const util = require("util");
+const stringify = require("json-stringify-safe");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
+var app = express(handleRequest);
+app.use(express.static(path.join(__dirname, '/public')));
 var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/db')));
 
 // Create a function which handles incoming requests and sends responses
 function handleRequest(req, res) {
@@ -24,7 +33,7 @@ function handleRequest(req, res) {
 
         case "/notes":
             return displayNotes(res);
-        
+
         default:
             return display404(path, res);
     }
@@ -33,7 +42,7 @@ function handleRequest(req, res) {
 // When someone visits the "http://localhost:3000/" path, this function is run.
 function displayRoot(res) {
     // Here we use the fs package to read our index.html file
-    fs.readFile(__dirname + "./public/index.html", function(err, data) {
+    fs.readFile(__dirname + "./public/index.html", function (err, data) {
         if (err) throw err;
         // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
         // an html file.
@@ -45,7 +54,7 @@ function displayRoot(res) {
 // When someone visits the "http://localhost:3000/notes" path, this function is run.
 function displayNotes(res) {
     // Here we use the fs package to read our index.html file
-    fs.readFile(__dirname + "./public/notes.html", function(err, data) {
+    fs.readFile(__dirname + "./public/notes.html", function (err, data) {
         if (err) throw err;
         // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
         // an html file.
@@ -67,9 +76,6 @@ function display404(url, res) {
     // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
     res.end(myHTML);
 }
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 
@@ -81,30 +87,91 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+function getDataFromFile() {
 
-// Displays all notes
-app.get("/api/notes", function (req, res) {
-    return res.json(notes);
-});
-
-// Displays a single note, or returns false
-app.get("/api/notes/:character", function (req, res) {
-    var chosen = req.params.notes;
-
-    console.log(chosen);
-
-    for (var i = 0; i < notes.length; i++) {
-        if (chosen === notes[i].routeName) {
-            return res.json(notes[i]);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
         }
-    }
+        noteArrayStr = JSON.parse(data);
+        console.log(noteArrayStr);
+    });
 
-    return res.json(false);
-});
+    // Displays all notes
+    app.get("/api/notes", function (request, response) {
+        // console.log(noteArrayStr[0].routeName);
+        noteArrayStr.forEach(element => {
+            // console.log(element.routeName);
+        })
+        return response.json(noteArrayStr);
+    });
+
+    // Displays a single note, or returns false
+    app.get("/api/notes/:character", function (request, response) {
+        var chosen = request.params.character;
+
+        console.log(chosen);
+
+        for (var i = 0; i < noteArrayStr.length; i++) {
+            if (chosen === noteArrayStr[i].routeName) {
+                return response.json(noteArrayStr[i]);
+            }
+        }
+        return response.json(false);
+    });
+
+    // Create new note - takes in JSON input
+    app.post("/api/notes", function (request, response) {
+        // console.log(noteArrayStr);
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newNote = request.body;
+
+        // Using a RegEx Pattern to remove spaces from newCharacter
+        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        const newId = noteArrayStr.length + 1;
+        // newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
+        newNote.routeName = newId.toString();
+
+        console.log(newNote);
+        noteArrayStr.push(newNote);
+        response.json(newNote);
+        console.log(noteArrayStr);
+        const newData = JSON.stringify(noteArrayStr, null, 4)
+
+        fs.writeFile("./db/db.json", newData, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    });
+    // Delete note from array
+    app.delete("/api/notes/:character", function (request, response) {
+        var chosen = request.params.character;
+        console.log(chosen);
+
+        for (var i = 0; i < noteArrayStr.length; i++) {
+            if (chosen === noteArrayStr[i].routeName) {
+                noteArrayStr.splice(i, 1);
+                console.log(noteArrayStr);
+                const newData = JSON.stringify(noteArrayStr, null, 4)
+
+                fs.writeFile("./db/db.json", newData, function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                });
+                return response.json(noteArrayStr);
+            }
+        }
+        return response.json(false);
+    });
+}
+
+getDataFromFile();
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
-
